daeae10d462ea4c70ba9029df1ecf61874ba28a0
Minjsi <alswltn09@naver.com>
2024-03-26 00:31:46 +0900

    remote add

---
+++ src/test/java/crawling/io/SpringCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package crawling.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
